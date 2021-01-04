237c9f7edd5c43d76f1bd34306d44bbfc4d4e94c
beto <jose.a.reyes.quevedo@accenture.com>
2021-01-03 20:25:19 -0600

    formating index file

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { mainRouterProvider } = require('./routers');
-const { errorHandler, logErrors, wrapErrors } = require('./utils/middleware/errorHandler.js');
-const { notFoundHandler } = require('./utils/middleware/notFoundHandler.js');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { mainRouterProvider } = require("./routers");
+const {
+  errorHandler,
+  logErrors,
+  wrapErrors,
+} = require("./utils/middleware/errorHandler.js");
+const { notFoundHandler } = require("./utils/middleware/notFoundHandler.js");
 
 const app = express();
 
-const { config } = require('./config/index');
+const { config } = require("./config/index");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,5 +26,5 @@ app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, () => {
-    console.log(`The App listen on port: ${config.port}`);
+  console.log(`The App listen on port: ${config.port}`);
 });
