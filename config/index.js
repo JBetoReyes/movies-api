512018c404e6890b30b76537f15c318e60b1d1d0
beto <jose.a.reyes.quevedo@accenture.com>
2021-01-03 20:23:12 -0600

    adding keys and jwt token to the config file

--- config/index.js
+++ config/index.js
@@ -1,13 +1,17 @@
-require('dotenv').config();
-console.log('env', process.env.NODE_ENV);
+require("dotenv").config();
 const config = {
-  dev: process.env.NODE_ENV !== 'production',
+  dev: process.env.NODE_ENV !== "production",
   port: process.env.PORT || 3000,
   cors: process.env.CORS,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME
+  dbName: process.env.DB_NAME,
+  authJWTSecret: process.env.AUTH_JWT_SECRET,
+  defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
+  defaultPublicPassword: process.env.DEFAULT_USER_PASSWORD,
+  publicApiKey: process.env.PUBLIC_API_KEY,
+  adminApiKey: process.env.ADMIN_API_KEY,
 };
 
 module.exports = { config };
